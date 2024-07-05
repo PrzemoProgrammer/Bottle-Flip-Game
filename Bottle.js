ff773719e75c75057d4a17c0ce88f4d7b3de8cd8
PrzemoProgrammer <110130880+PrzemoProgrammer@users.noreply.github.com>
2024-07-05 17:07:23 +0200

    update

--- Bottle.js
+++ Bottle.js
@@ -1,110 +1,126 @@
-const _0x1548df = _0x3b57;
-(function (_0x226cbc, _0x348801) {
-  const _0x1dafc9 = _0x3b57,
-    _0x33c845 = _0x226cbc();
-  while (!![]) {
-    try {
-      const _0x2e6854 =
-        (parseInt(_0x1dafc9(0x1fd)) / 0x1) *
-          (-parseInt(_0x1dafc9(0x202)) / 0x2) +
-        -parseInt(_0x1dafc9(0x1f0)) / 0x3 +
-        -parseInt(_0x1dafc9(0x1ed)) / 0x4 +
-        parseInt(_0x1dafc9(0x1f1)) / 0x5 +
-        parseInt(_0x1dafc9(0x1f6)) / 0x6 +
-        (-parseInt(_0x1dafc9(0x1e9)) / 0x7) *
-          (-parseInt(_0x1dafc9(0x204)) / 0x8) +
-        (parseInt(_0x1dafc9(0x1e7)) / 0x9) * (parseInt(_0x1dafc9(0x1f7)) / 0xa);
-      if (_0x2e6854 === _0x348801) break;
-      else _0x33c845["push"](_0x33c845["shift"]());
-    } catch (_0xfe0e91) {
-      _0x33c845["push"](_0x33c845["shift"]());
-    }
-  }
-})(_0x3e81, 0xec6c9);
-function _0x3b57(_0x11e6bc, _0x36ddef) {
-  const _0x3e81e7 = _0x3e81();
+const _0x597a5b = _0x1308;
+function _0x1308(_0x5c3199, _0x3a1f27) {
+  const _0x19e318 = _0x19e3();
   return (
-    (_0x3b57 = function (_0x3b57ce, _0x1d584e) {
-      _0x3b57ce = _0x3b57ce - 0x1e6;
-      let _0x137e75 = _0x3e81e7[_0x3b57ce];
-      return _0x137e75;
+    (_0x1308 = function (_0x130898, _0x1311c1) {
+      _0x130898 = _0x130898 - 0xec;
+      let _0x566058 = _0x19e318[_0x130898];
+      return _0x566058;
     }),
-    _0x3b57(_0x11e6bc, _0x36ddef)
+    _0x1308(_0x5c3199, _0x3a1f27)
   );
 }
-class Bottle extends Phaser[_0x1548df(0x1ee)][_0x1548df(0x1fa)] {
-  constructor(_0xe2b642, _0x1b97f2, _0x358a0e, _0x52768c) {
-    const _0x4efe58 = _0x1548df;
-    super(_0xe2b642, _0x1b97f2, _0x358a0e, _0x52768c),
-      (this[_0x4efe58(0x1e8)] = _0xe2b642),
-      _0xe2b642[_0x4efe58(0x200)][_0x4efe58(0x1fb)](this),
-      _0xe2b642[_0x4efe58(0x1ec)][_0x4efe58(0x200)][_0x4efe58(0x1eb)](this),
-      this[_0x4efe58(0x203)](0.4),
-      this[_0x4efe58(0x1ea)](0.009),
-      this[_0x4efe58(0x1f9)](0xa),
-      this[_0x4efe58(0x1f8)](),
-      (this[_0x4efe58(0x1ef)] = !![]),
-      (this[_0x4efe58(0x1ff)] = ![]),
-      (this[_0x4efe58(0x206)] = ![]);
+(function (_0x77a96d, _0x598e5f) {
+  const _0x53d609 = _0x1308,
+    _0x27bddb = _0x77a96d();
+  while (!![]) {
+    try {
+      const _0xb7d3fc =
+        (-parseInt(_0x53d609(0x107)) / 0x1) *
+          (parseInt(_0x53d609(0xf0)) / 0x2) +
+        (parseInt(_0x53d609(0xed)) / 0x3) * (parseInt(_0x53d609(0xfa)) / 0x4) +
+        -parseInt(_0x53d609(0xee)) / 0x5 +
+        parseInt(_0x53d609(0xfd)) / 0x6 +
+        -parseInt(_0x53d609(0x108)) / 0x7 +
+        -parseInt(_0x53d609(0x101)) / 0x8 +
+        (-parseInt(_0x53d609(0xfb)) / 0x9) *
+          (-parseInt(_0x53d609(0x103)) / 0xa);
+      if (_0xb7d3fc === _0x598e5f) break;
+      else _0x27bddb["push"](_0x27bddb["shift"]());
+    } catch (_0x5c4cee) {
+      _0x27bddb["push"](_0x27bddb["shift"]());
+    }
   }
-  [_0x1548df(0x201)](_0x1fdc83) {
-    const _0x5939bf = _0x1548df;
-    if (!this["canFlip"]) return;
-    this["canFlip"] = ![];
-    let _0x46eb47 =
-      (this[_0x5939bf(0x1e8)][_0x5939bf(0x1fc)] - _0x1fdc83) / 0x14;
-    if (_0x46eb47 > 0x23) _0x46eb47 = 0x23;
-    this[_0x5939bf(0x1f3)](0x0, -_0x46eb47),
-      this[_0x5939bf(0x1f4)](_0x46eb47 / 0x12c);
+})(_0x19e3, 0x1e502);
+class Bottle extends Phaser[_0x597a5b(0xf7)][_0x597a5b(0xf5)] {
+  constructor(_0x394115, _0x177988, _0x3451fe, _0x1a722b) {
+    const _0x161cc0 = _0x597a5b;
+    super(_0x394115, _0x177988, _0x3451fe, _0x1a722b),
+      (this["scene"] = _0x394115),
+      _0x394115[_0x161cc0(0xfe)][_0x161cc0(0xf8)](this),
+      this[_0x161cc0(0x109)](),
+      (this[_0x161cc0(0xec)] = ![]),
+      (this[_0x161cc0(0x100)] = ![]),
+      (this[_0x161cc0(0xff)] = ![]);
   }
-  [_0x1548df(0x1f2)]() {
-    const _0x38eab6 = _0x1548df;
+  [_0x597a5b(0xf4)]() {
+    const _0xf3254a = _0x597a5b;
+    this[_0xf3254a(0x106)][_0xf3254a(0x104)][_0xf3254a(0xfe)]["gameObject"](
+      this
+    ),
+      this["setFriction"](0.4),
+      this[_0xf3254a(0xfc)](0.009),
+      this[_0xf3254a(0x102)](0xa),
+      this[_0xf3254a(0x109)]();
+  }
+  ["flip"](_0x7ae1d1) {
+    const _0xc6e3f2 = _0x597a5b;
+    if (!this[_0xc6e3f2(0xec)]) return;
+    this[_0xc6e3f2(0xec)] = ![];
+    let _0x49872f =
+      (this[_0xc6e3f2(0x106)][_0xc6e3f2(0xf2)] - _0x7ae1d1) / 0x14;
+    if (_0x49872f > 0x23) _0x49872f = 0x23;
+    this["setVelocity"](0x0, -_0x49872f),
+      this["setAngularVelocity"](_0x49872f / 0x12c);
+  }
+  ["isStanding"]() {
+    const _0x24dbda = _0x597a5b;
     return (
-      Math[_0x38eab6(0x1e6)](this[_0x38eab6(0x205)]) < 0.65 &&
-      Math["abs"](this[_0x38eab6(0x1f5)]["angularVelocity"]) < 0x2 &&
-      Math[_0x38eab6(0x1e6)](this[_0x38eab6(0x1f5)][_0x38eab6(0x1fe)]["y"]) <
-        0x2
+      Math[_0x24dbda(0xf6)](this["angle"]) < 0.65 &&
+      Math[_0x24dbda(0xf6)](this["body"][_0x24dbda(0xf3)]) < 0x2 &&
+      Math[_0x24dbda(0xf6)](this[_0x24dbda(0x105)][_0x24dbda(0xf1)]["y"]) < 0x2
     );
   }
+  [_0x597a5b(0xf9)](_0x325e17) {
+    const _0x480435 = _0x597a5b;
+    this["setScale"](0.1),
+      this[_0x480435(0x106)][_0x480435(0xef)][_0x480435(0xfe)]({
+        targets: this,
+        ease: "Back.out",
+        duration: 0x12c,
+        scale: 0x1,
+        onComplete: () => {
+          const _0x316ad0 = _0x480435;
+          (this[_0x316ad0(0xec)] = !![]), _0x325e17();
+        },
+      });
+  }
 }
-function _0x3e81() {
-  const _0x13c9c4 = [
-    "setAngularVelocity",
-    "body",
-    "3692856qYhIvq",
-    "2933990sSMRtu",
-    "setInteractive",
-    "setFrictionStatic",
-    "Sprite",
-    "existing",
-    "startDragY",
-    "7QhMbEz",
-    "velocity",
-    "hasFlippedInAir",
-    "add",
-    "flip",
-    "446742VKWNBB",
-    "setFriction",
-    "16WQUPUs",
-    "angle",
-    "isTouched",
-    "abs",
-    "18IWPOFm",
-    "scene",
-    "5242685QUpnMT",
+function _0x19e3() {
+  const _0x269889 = [
+    "createTween",
+    "8OnaMSZ",
+    "474678CAkLSt",
     "setFrictionAir",
-    "gameObject",
+    "1099134UKsFeO",
+    "add",
+    "isTouched",
+    "hasFlippedInAir",
+    "1657424NClAql",
+    "setFrictionStatic",
+    "70YUYpbG",
     "matter",
-    "2475848FyMegB",
-    "GameObjects",
+    "body",
+    "scene",
+    "2IgXyte",
+    "1201102PAvZMf",
+    "setInteractive",
     "canFlip",
-    "3143388vmAWAc",
-    "7492820aOqSxs",
-    "isStanding",
-    "setVelocity",
+    "46572PhrwNM",
+    "331705tTuwKZ",
+    "tweens",
+    "14164nmrbWP",
+    "velocity",
+    "startDragY",
+    "angularVelocity",
+    "addBody",
+    "Sprite",
+    "abs",
+    "GameObjects",
+    "existing",
   ];
-  _0x3e81 = function () {
-    return _0x13c9c4;
+  _0x19e3 = function () {
+    return _0x269889;
   };
-  return _0x3e81();
+  return _0x19e3();
 }
--- PlayScene.js
+++ PlayScene.js
@@ -1,246 +1,352 @@
-function _0x51f7(_0x586423, _0x15cdff) {
-  const _0x44a8fe = _0x44a8();
-  return (
-    (_0x51f7 = function (_0x51f718, _0x1980f6) {
-      _0x51f718 = _0x51f718 - 0x123;
-      let _0x2d29cf = _0x44a8fe[_0x51f718];
-      return _0x2d29cf;
-    }),
-    _0x51f7(_0x586423, _0x15cdff)
-  );
-}
-function _0x44a8() {
-  const _0x39678b = [
-    "60px",
-    "displayHeight",
-    "getItem",
-    "startDragY",
-    "./assets/bottle.png",
-    "abs",
-    "preload",
-    "scene",
-    "text",
-    "config",
-    "update",
-    "restart",
-    "setDisplaySize",
-    "height",
-    "addScoreText",
-    "5746070PIFhAA",
-    "Scene",
-    "setOrigin",
-    "15773870SGgJrW",
-    "time",
-    "1842694QBdJwP",
-    "floorBody",
+const _0x4b6855 = _0x1be5;
+function _0x3fb0() {
+  const _0x3b6051 = [
+    "42JEsjXm",
     "bestScore",
-    "91887wDYOPy",
-    "28FcjTDn",
-    "scoreText",
-    "flip",
-    "2244324dAKnAJ",
-    "setOnCollideActive",
-    "addFloor",
-    "rectangle",
-    "TEST",
     "hasFlippedInAir",
-    "PlayScene",
-    "./assets/background.png",
-    "isTouched",
-    "63reNKyu",
-    "hitFloor",
-    "add",
-    "bottle",
-    "878856xtywJY",
-    "body",
-    "delayedCall",
-    "destroy",
-    "background",
-    "128lNhBwa",
-    "image",
-    "floor",
+    "30QleIOI",
+    "Scene",
+    "./assets/floor.png",
+    "canFlip",
+    "matter",
+    "addBodies",
+    "getItem",
+    "3027801TTTNKE",
     "velocity",
-    "load",
-    "addDragEvents",
-    "game",
+    "handleCollide",
+    "update",
     "SCORE:\x20",
-    "isStanding",
+    "dragend",
+    "style",
+    "PlayScene",
+    "tweens",
     "dragstart",
-    "displayWidth",
-    "737594qZHwZB",
+    "addFloor",
+    "hideLoadingScreen",
+    "floorBody",
     "Georgia,\x20\x22Goudy\x20Bookletter\x201911\x22,\x20Times,\x20serif",
-    "input",
+    "BEST:\x20",
+    "addDragEvents",
+    "Arial,\x20\x22Goudy\x20Bookletter\x201911\x22,\x20Times,\x20serif",
+    "text",
+    "floor",
+    "hitFloor",
+    "17910234XLavZd",
+    "scale",
+    "4xuOGSb",
+    "TEST",
+    "image",
+    "setDraggable",
+    "7814163bVdblr",
+    "scene",
+    "height",
+    "flip",
+    "game",
+    "addBackground",
+    "displayWidth",
+    "Back.in",
+    "config",
+    "soft",
+    "/assets/bottle.png",
+    "body",
+    "background",
+    "./assets/background.png",
+    "addBottle",
+    "isStanding",
+    "addScoreText",
+    "isTouched",
+    "1231224LAGnNL",
+    "startOpenAnimTween",
+    "remove",
+    "load",
+    "setOrigin",
+    "destroy",
+    "scoreText",
+    "restart",
+    "addBestScoreText",
+    "addBody",
     "log",
+    "addTestMode",
+    "4301124DPVBzY",
+    "delayedCall",
+    "bestScoreText",
+    "DESTROYED",
+    "setItem",
+    "add",
+    "setOnCollideActive",
+    "100px",
+    "bottle",
+    "Back.out",
+    "time",
+    "createTween",
+    "getElementById",
+    "startDragY",
+    "create",
+    "585505QSWivK",
+    "579540aCSKBT",
+    "scale(",
+    "setScale",
+    "input",
+    "width",
   ];
-  _0x44a8 = function () {
-    return _0x39678b;
+  _0x3fb0 = function () {
+    return _0x3b6051;
   };
-  return _0x44a8();
+  return _0x3fb0();
 }
-const _0x28aca7 = _0x51f7;
-(function (_0x586f39, _0x1b3f06) {
-  const _0x1c65dc = _0x51f7,
-    _0x2db3de = _0x586f39();
+(function (_0x7683e, _0x22b467) {
+  const _0x16e9d0 = _0x1be5,
+    _0x2835dd = _0x7683e();
   while (!![]) {
     try {
-      const _0x469123 =
-        -parseInt(_0x1c65dc(0x155)) / 0x1 +
-        -parseInt(_0x1c65dc(0x131)) / 0x2 +
-        (-parseInt(_0x1c65dc(0x134)) / 0x3) *
-          (parseInt(_0x1c65dc(0x14a)) / 0x4) +
-        parseInt(_0x1c65dc(0x12c)) / 0x5 +
-        (-parseInt(_0x1c65dc(0x138)) / 0x6) *
-          (-parseInt(_0x1c65dc(0x135)) / 0x7) +
-        (-parseInt(_0x1c65dc(0x145)) / 0x8) *
-          (parseInt(_0x1c65dc(0x141)) / 0x9) +
-        parseInt(_0x1c65dc(0x12f)) / 0xa;
-      if (_0x469123 === _0x1b3f06) break;
-      else _0x2db3de["push"](_0x2db3de["shift"]());
-    } catch (_0x68089f) {
-      _0x2db3de["push"](_0x2db3de["shift"]());
+      const _0x58559c =
+        (parseInt(_0x16e9d0(0x1a3)) / 0x1) *
+          (-parseInt(_0x16e9d0(0x17e)) / 0x2) +
+        -parseInt(_0x16e9d0(0x18d)) / 0x3 +
+        -parseInt(_0x16e9d0(0x16e)) / 0x4 +
+        (-parseInt(_0x16e9d0(0x17d)) / 0x5) *
+          (parseInt(_0x16e9d0(0x183)) / 0x6) +
+        -parseInt(_0x16e9d0(0x1a7)) / 0x7 +
+        -parseInt(_0x16e9d0(0x162)) / 0x8 +
+        (-parseInt(_0x16e9d0(0x1a1)) / 0x9) *
+          (-parseInt(_0x16e9d0(0x186)) / 0xa);
+      if (_0x58559c === _0x22b467) break;
+      else _0x2835dd["push"](_0x2835dd["shift"]());
+    } catch (_0x55d819) {
+      _0x2835dd["push"](_0x2835dd["shift"]());
     }
   }
-})(_0x44a8, 0xc6e9d);
+})(_0x3fb0, 0x9b673);
+function _0x1be5(_0x115a50, _0x28c250) {
+  const _0x3fb076 = _0x3fb0();
+  return (
+    (_0x1be5 = function (_0x1be5c0, _0x299c46) {
+      _0x1be5c0 = _0x1be5c0 - 0x15a;
+      let _0x34bcd3 = _0x3fb076[_0x1be5c0];
+      return _0x34bcd3;
+    }),
+    _0x1be5(_0x115a50, _0x28c250)
+  );
+}
 let score = 0x0,
-  bestScore = Number(localStorage[_0x28aca7(0x15b)](_0x28aca7(0x133))) || 0x0;
-class PlayScene extends Phaser[_0x28aca7(0x12d)] {
+  bestScore = Number(localStorage[_0x4b6855(0x18c)](_0x4b6855(0x184))) || 0x0;
+class PlayScene extends Phaser[_0x4b6855(0x187)] {
   constructor() {
-    const _0x922d1c = _0x28aca7;
-    super(_0x922d1c(0x13e));
+    const _0x2dd043 = _0x4b6855;
+    super(_0x2dd043(0x194));
   }
-  [_0x28aca7(0x123)]() {
-    const _0x47ccfb = _0x28aca7;
-    this[_0x47ccfb(0x14e)][_0x47ccfb(0x14b)](
-      _0x47ccfb(0x149),
-      _0x47ccfb(0x13f)
+  ["preload"]() {
+    const _0x39ff8f = _0x4b6855;
+    this[_0x39ff8f(0x165)][_0x39ff8f(0x1a5)](
+      _0x39ff8f(0x15c),
+      _0x39ff8f(0x15d)
     ),
-      this[_0x47ccfb(0x14e)]["image"](_0x47ccfb(0x144), _0x47ccfb(0x15d)),
-      this[_0x47ccfb(0x14e)]["image"](_0x47ccfb(0x14c), "./assets/floor.png");
+      this["load"][_0x39ff8f(0x1a5)]("bottle", _0x39ff8f(0x15a)),
+      this[_0x39ff8f(0x165)][_0x39ff8f(0x1a5)](
+        _0x39ff8f(0x19f),
+        _0x39ff8f(0x188)
+      );
   }
-  ["create"]() {
-    const _0x13fbb0 = _0x28aca7;
-    (this["gw"] = this[_0x13fbb0(0x150)][_0x13fbb0(0x126)]["width"]),
-      (this["gh"] = this[_0x13fbb0(0x150)][_0x13fbb0(0x126)][_0x13fbb0(0x12a)]),
-      (this["startDragY"] = 0x0),
-      this["addBackground"](),
-      this[_0x13fbb0(0x12b)]();
-    const _0x351a06 = {
-      fontFamily:
-        "Georgia,\x20\x22Goudy\x20Bookletter\x201911\x22,\x20Times,\x20serif",
-      fontSize: "100px",
+  [_0x4b6855(0x17c)](_0x2e1228) {
+    const _0xbb3b9d = _0x4b6855;
+    (this["gw"] = this["game"][_0xbb3b9d(0x1af)][_0xbb3b9d(0x182)]),
+      (this["gh"] = this[_0xbb3b9d(0x1ab)][_0xbb3b9d(0x1af)]["height"]),
+      (this[_0xbb3b9d(0x17b)] = 0x0),
+      (this[_0xbb3b9d(0x15c)] = this[_0xbb3b9d(0x1ac)]()),
+      (this["scoreText"] = this[_0xbb3b9d(0x160)]()),
+      (this[_0xbb3b9d(0x170)] = this[_0xbb3b9d(0x16a)]()),
+      (this[_0xbb3b9d(0x19f)] = this["addFloor"]()),
+      (this["bottle"] = this[_0xbb3b9d(0x15e)]()),
+      this[_0xbb3b9d(0x19c)]();
+    const _0x126f96 = () => {
+      const _0x41de7a = _0xbb3b9d;
+      _0x2e1228 != _0x41de7a(0x1b0)
+        ? this[_0x41de7a(0x163)]()
+        : this[_0x41de7a(0x176)][_0x41de7a(0x179)](() => {
+            const _0x254a0e = _0x41de7a;
+            this[_0x254a0e(0x18b)](), this[_0x254a0e(0x18f)]();
+          });
     };
-    this["add"]
-      [_0x13fbb0(0x125)](
+    _0x2e1228 != _0xbb3b9d(0x1b0)
+      ? this["hideLoadingScreen"](_0x126f96)
+      : _0x126f96(),
+      this[_0xbb3b9d(0x16d)]();
+  }
+  [_0x4b6855(0x190)]() {
+    const _0x2c46c3 = _0x4b6855;
+    Math["abs"](this[_0x2c46c3(0x176)]["angle"]) > 0xa0 &&
+      (this[_0x2c46c3(0x176)][_0x2c46c3(0x185)] = !![]);
+  }
+  [_0x4b6855(0x1ac)]() {
+    const _0x1e3611 = _0x4b6855;
+    return this[_0x1e3611(0x173)]
+      [_0x1e3611(0x1a5)](0x0, 0x0, _0x1e3611(0x15c))
+      [_0x1e3611(0x166)](0x0, 0x0)
+      ["setDisplaySize"](this["gw"], this["gh"]);
+  }
+  [_0x4b6855(0x160)]() {
+    const _0x5abbdd = _0x4b6855,
+      _0x4f33c1 = { fontFamily: _0x5abbdd(0x19d), fontSize: "60px" };
+    return this[_0x5abbdd(0x173)]
+      [_0x5abbdd(0x19e)](0xa, 0xa, _0x5abbdd(0x191) + score, _0x4f33c1)
+      [_0x5abbdd(0x166)](0x0, 0x0);
+  }
+  [_0x4b6855(0x16a)]() {
+    const _0x449b3b = _0x4b6855,
+      _0x5c18a1 = { fontFamily: _0x449b3b(0x19d), fontSize: "60px" };
+    return this[_0x449b3b(0x173)]
+      ["text"](this["gw"] - 0xa, 0xa, _0x449b3b(0x19b) + bestScore, _0x5c18a1)
+      [_0x449b3b(0x166)](0x1, 0x0);
+  }
+  [_0x4b6855(0x197)]() {
+    const _0x53d114 = _0x4b6855,
+      _0x347aa3 = this[_0x53d114(0x173)]
+        [_0x53d114(0x1a5)](0x1cc, this["gh"], "floor")
+        [_0x53d114(0x166)](0.5, 0x1);
+    return _0x347aa3;
+  }
+  [_0x4b6855(0x15e)]() {
+    const _0x47a20b = _0x4b6855,
+      _0x5a40d4 = new Bottle(
+        this,
         this["gw"] / 0x2,
-        this["gh"] / 0x2 - 0x64,
-        _0x13fbb0(0x13c),
-        _0x351a06
-      )
-      [_0x13fbb0(0x12e)](0x0, 0x0),
-      this[_0x13fbb0(0x143)]
-        [_0x13fbb0(0x125)](
-          this["gw"] / 0x2,
-          this["gh"] / 0x2 + 0x64,
-          _0x13fbb0(0x13c),
-          _0x351a06
-        )
-        ["setOrigin"](0x0, 0x0),
-      this[_0x13fbb0(0x13a)](),
-      this["addBottle"](),
-      this[_0x13fbb0(0x14f)](),
-      this[_0x13fbb0(0x130)][_0x13fbb0(0x147)](0x2328, () => {
-        const _0xdcbee4 = _0x13fbb0;
-        console[_0xdcbee4(0x158)]("DESTROYED"),
-          this[_0xdcbee4(0x149)]["destroy"](),
-          this["bottle"][_0xdcbee4(0x148)]();
-      });
+        this["floor"]["y"],
+        _0x47a20b(0x176)
+      );
+    return (
+      (_0x5a40d4["y"] -= _0x5a40d4[_0x47a20b(0x1a9)] / 0x2 + 0x92),
+      this[_0x47a20b(0x181)][_0x47a20b(0x1a6)](_0x5a40d4),
+      _0x5a40d4
+    );
   }
-  [_0x28aca7(0x127)]() {
-    const _0x2f1001 = _0x28aca7;
-    Math[_0x2f1001(0x15e)](this[_0x2f1001(0x144)]["angle"]) > 0xa0 &&
-      (this[_0x2f1001(0x144)][_0x2f1001(0x13d)] = !![]);
-  }
-  [_0x28aca7(0x142)]() {
-    const _0x4cd5f6 = _0x28aca7;
-    this["bottle"][_0x4cd5f6(0x152)]() &&
-      this[_0x4cd5f6(0x144)]["isTouched"] &&
-      ((this[_0x4cd5f6(0x144)][_0x4cd5f6(0x140)] = ![]),
-      this[_0x4cd5f6(0x130)][_0x4cd5f6(0x147)](0x7d0, () => {
-        const _0x5bf392 = _0x4cd5f6;
-        if (this[_0x5bf392(0x144)][_0x5bf392(0x152)]())
-          this[_0x5bf392(0x144)]["hasFlippedInAir"] && score++,
+  [_0x4b6855(0x1a0)]() {
+    const _0x44f8c7 = _0x4b6855;
+    this["bottle"][_0x44f8c7(0x15f)]() &&
+      this[_0x44f8c7(0x176)][_0x44f8c7(0x161)] &&
+      ((this[_0x44f8c7(0x176)]["isTouched"] = ![]),
+      this[_0x44f8c7(0x178)][_0x44f8c7(0x16f)](0x7d0, () => {
+        const _0xa32650 = _0x44f8c7;
+        if (this[_0xa32650(0x176)][_0xa32650(0x15f)]())
+          this[_0xa32650(0x176)][_0xa32650(0x185)] && score++,
             score > bestScore &&
               ((bestScore = score),
-              localStorage["setItem"](_0x5bf392(0x133), bestScore)),
-            this[_0x5bf392(0x124)][_0x5bf392(0x128)]();
+              localStorage[_0xa32650(0x172)](_0xa32650(0x184), bestScore)),
+            this[_0xa32650(0x1a8)][_0xa32650(0x169)]("soft");
         else
-          this["bottle"]["body"][_0x5bf392(0x14d)]["y"] === 0x0 &&
-            this["bottle"][_0x5bf392(0x140)] &&
-            this[_0x5bf392(0x124)][_0x5bf392(0x128)]();
+          this["bottle"][_0xa32650(0x15b)][_0xa32650(0x18e)]["y"] === 0x0 &&
+            this[_0xa32650(0x176)][_0xa32650(0x161)] &&
+            this[_0xa32650(0x1a8)][_0xa32650(0x169)]("soft");
       })),
-      this[_0x4cd5f6(0x144)][_0x4cd5f6(0x146)]["velocity"]["y"] === 0x0 &&
+      this[_0x44f8c7(0x176)][_0x44f8c7(0x15b)]["velocity"]["y"] === 0x0 &&
         this["bottle"]["isTouched"] &&
-        this["scene"][_0x4cd5f6(0x128)]();
+        this[_0x44f8c7(0x1a8)][_0x44f8c7(0x169)](_0x44f8c7(0x1b0));
   }
-  ["addBackground"]() {
-    const _0x2e5575 = _0x28aca7;
-    this[_0x2e5575(0x149)] = this[_0x2e5575(0x143)]
-      ["image"](0x0, 0x0, _0x2e5575(0x149))
-      ["setOrigin"](0x0, 0x0)
-      [_0x2e5575(0x129)](this["gw"], this["gh"]);
+  [_0x4b6855(0x19c)]() {
+    const _0x432494 = _0x4b6855;
+    this[_0x432494(0x181)]["on"](
+      _0x432494(0x196),
+      (_0x298691) => (this[_0x432494(0x17b)] = _0x298691["y"])
+    ),
+      this["input"]["on"](_0x432494(0x192), (_0x2eeb85) => {
+        const _0x32b131 = _0x432494;
+        this[_0x32b131(0x176)][_0x32b131(0x1aa)](_0x2eeb85["y"]),
+          (this[_0x32b131(0x176)][_0x32b131(0x161)] = !![]);
+      });
   }
-  [_0x28aca7(0x12b)]() {
-    const _0xc57457 = _0x28aca7,
-      _0x490921 = { fontFamily: _0xc57457(0x156), fontSize: _0xc57457(0x159) };
-    (this[_0xc57457(0x136)] = this[_0xc57457(0x143)]
-      [_0xc57457(0x125)](0xa, 0xa, _0xc57457(0x151) + score, _0x490921)
-      [_0xc57457(0x12e)](0x0, 0x0)),
-      (this["bestScoreText"] = this["add"]
-        [_0xc57457(0x125)](
-          this["gw"] - 0xa,
-          0xa,
-          "BEST:\x20" + bestScore,
-          _0x490921
-        )
-        [_0xc57457(0x12e)](0x1, 0x0));
-  }
-  [_0x28aca7(0x13a)]() {
-    const _0x34f4cc = _0x28aca7;
-    (this[_0x34f4cc(0x14c)] = this[_0x34f4cc(0x143)]
-      ["image"](0x0, this["gh"], _0x34f4cc(0x14c))
-      [_0x34f4cc(0x12e)](0x0, 0x1)),
-      (this[_0x34f4cc(0x132)] = this["matter"][_0x34f4cc(0x143)][
-        _0x34f4cc(0x13b)
+  [_0x4b6855(0x18b)]() {
+    const _0x3404fe = _0x4b6855;
+    this["bottle"][_0x3404fe(0x16b)](),
+      (this[_0x3404fe(0x199)] = this[_0x3404fe(0x18a)][_0x3404fe(0x173)][
+        "rectangle"
       ](
         this["gw"] / 0x2,
         this["gh"] - 0x28,
-        this[_0x34f4cc(0x14c)][_0x34f4cc(0x154)],
-        this["floor"][_0x34f4cc(0x15a)],
+        this[_0x3404fe(0x19f)][_0x3404fe(0x1ad)],
+        this["floor"]["displayHeight"],
         { isStatic: !![] }
       ));
   }
-  ["addBottle"]() {
-    const _0x1cad8c = _0x28aca7;
-    (this["bottle"] = new Bottle(
-      this,
-      this["gw"] / 0x2,
-      this[_0x1cad8c(0x14c)]["y"],
-      _0x1cad8c(0x144)
-    )),
-      (this["bottle"]["y"] -= this["bottle"][_0x1cad8c(0x12a)] / 0x2),
-      this[_0x1cad8c(0x157)]["setDraggable"](this[_0x1cad8c(0x144)]),
-      this["bottle"][_0x1cad8c(0x139)](() => this[_0x1cad8c(0x142)]());
+  [_0x4b6855(0x18f)]() {
+    const _0x39f9eb = _0x4b6855;
+    this[_0x39f9eb(0x176)][_0x39f9eb(0x174)](() => this["hitFloor"]());
   }
-  [_0x28aca7(0x14f)]() {
-    const _0x3c94ea = _0x28aca7;
-    this[_0x3c94ea(0x157)]["on"](
-      _0x3c94ea(0x153),
-      (_0x50e1c1) => (this[_0x3c94ea(0x15c)] = _0x50e1c1["y"])
-    ),
-      this["input"]["on"]("dragend", (_0x4d1c41) => {
-        const _0x53b910 = _0x3c94ea;
-        this[_0x53b910(0x144)][_0x53b910(0x137)](_0x4d1c41["y"]),
-          (this[_0x53b910(0x144)][_0x53b910(0x140)] = !![]);
+  [_0x4b6855(0x163)]() {
+    const _0x2a175a = _0x4b6855;
+    this[_0x2a175a(0x176)][_0x2a175a(0x180)](0.1),
+      this["floor"]["setScale"](0.1),
+      this[_0x2a175a(0x168)][_0x2a175a(0x180)](0.1),
+      this["bestScoreText"]["setScale"](0.1),
+      this[_0x2a175a(0x195)]["add"]({
+        targets: this[_0x2a175a(0x176)],
+        ease: _0x2a175a(0x177),
+        duration: 0x3e8,
+        scale: 0x1,
+        onComplete: () => {
+          const _0x4b2c2d = _0x2a175a;
+          (this[_0x4b2c2d(0x176)][_0x4b2c2d(0x189)] = !![]),
+            this[_0x4b2c2d(0x18b)](),
+            this[_0x4b2c2d(0x18f)]();
+        },
+      }),
+      this[_0x2a175a(0x195)][_0x2a175a(0x173)]({
+        targets: this["floor"],
+        ease: _0x2a175a(0x177),
+        duration: 0x320,
+        scale: 0x1,
+      }),
+      this["tweens"][_0x2a175a(0x173)]({
+        targets: [this["bestScoreText"], this["scoreText"]],
+        ease: _0x2a175a(0x177),
+        duration: 0x2bc,
+        scale: 0x1,
+      });
+  }
+  [_0x4b6855(0x198)](_0x22d764) {
+    const _0x36e624 = _0x4b6855,
+      _0x165e38 = document[_0x36e624(0x17a)]("loading_page");
+    this[_0x36e624(0x195)]["add"]({
+      targets: _0x165e38,
+      scale: { from: 0x1, to: 0x0 },
+      ease: _0x36e624(0x1ae),
+      duration: 0x258,
+      onUpdate: (_0x2fa672) => {
+        const _0x2838d5 = _0x36e624,
+          _0x212c77 = _0x2fa672["getValue"]();
+        _0x165e38[_0x2838d5(0x193)]["transform"] =
+          _0x2838d5(0x17f) + _0x212c77 + ")";
+      },
+      onComplete: () => {
+        const _0x5ba4a4 = _0x36e624;
+        _0x22d764(),
+          _0x165e38[_0x5ba4a4(0x164)](),
+          this[_0x5ba4a4(0x1a2)]["resize"](this["gw"], this["gh"]);
+      },
+    });
+  }
+  [_0x4b6855(0x16d)]() {
+    const _0x53b071 = _0x4b6855,
+      _0x461bd6 = { fontFamily: _0x53b071(0x19a), fontSize: _0x53b071(0x175) };
+    this["add"]
+      [_0x53b071(0x19e)](
+        this["gw"] / 0x2,
+        this["gh"] / 0x2 - 0x64,
+        _0x53b071(0x1a4),
+        _0x461bd6
+      )
+      [_0x53b071(0x166)](0x0, 0x0),
+      this[_0x53b071(0x173)]
+        [_0x53b071(0x19e)](
+          this["gw"] / 0x2,
+          this["gh"] / 0x2 + 0x64,
+          "TEST",
+          _0x461bd6
+        )
+        [_0x53b071(0x166)](0x0, 0x0),
+      this["time"][_0x53b071(0x16f)](0x2328, () => {
+        const _0x1b1c54 = _0x53b071;
+        console[_0x1b1c54(0x16c)](_0x1b1c54(0x171)),
+          this["background"][_0x1b1c54(0x167)](),
+          this[_0x1b1c54(0x176)]["destroy"]();
       });
   }
 }
